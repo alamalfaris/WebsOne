8c93cc616f53ddd98c4d370e066bcfaa12e476e8
Alam Alfaris <61654794+alamalfaris@users.noreply.github.com>
2023-06-29 14:36:34 +0700

    Initial commit

---
+++ WebsOne/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
